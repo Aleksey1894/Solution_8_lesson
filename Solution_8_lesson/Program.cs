2371402b74043e6f515109902d4d49d3ee96ae84
Aleksey1894 <oleksii.litvinchuk94@gmail.com>
2023-09-17 16:35:08 +0300

    change made

--- Solution_8_lesson/Program.cs
+++ Solution_8_lesson/Program.cs
@@ -10,26 +10,26 @@ namespace Solution_8_lesson
     {
         static bool Compare(string a, string b)
         {
-            if ((object)a == (object)b)
-            {
-                return true;
-            }
 
-            else if ((object)a == null || (object)b == null)
-            {
+            if ((object)a == null || (object)b == null)
                 return false;
-            }
-
+            
             else if (a.Length != b.Length)
                 return false;
 
-            return Compare(a, b);
+            for(int i = 0; i <a.Length; i++)
+            {
+                if (a[i] != b[i])
+                    return false;
+            }
+            return (true);
+                      
         }
 
         static void Main(string[] args)
         {
             string a = "real";
-            string b = "madrid";
+            string b = "Real";
 
             bool result = Compare(a, b);
 
