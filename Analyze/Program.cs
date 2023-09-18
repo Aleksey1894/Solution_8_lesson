17f13bdea9bd09df1a610f305f5e59b269906176
Aleksey1894 <oleksii.litvinchuk94@gmail.com>
2023-09-19 00:18:10 +0300

    change_made_2

--- Analyze/Program.cs
+++ Analyze/Program.cs
@@ -8,22 +8,25 @@ namespace Analyze
 {
     internal class Program
     {
-        public static void Analyze(string str, out int letter, out int numeric, out int spec)
-        {
-            letter = str.Count(char.IsLetter);
-            numeric = str.Count(char.IsDigit);
-            spec = str.Length - letter - numeric;
+      //  public static void Analyze(string str, out int letter, out int numeric, out int spec)
+       // {
+         //   letter = str.Count(char.IsLetter);
+           // numeric = str.Count(char.IsDigit);
+           // spec = str.Length - letter - numeric;
 
-        }
+       // }
+
         static void Main(string[] args)
         {
             string str = "dynamo100%";
             int letter, numeric, spec;
             Analyze(str, out letter, out numeric, out spec);
 
-            Console.WriteLine("Number of letters: " + letter);
-            Console.WriteLine("Amount of numbers: " + numeric);
-            Console.WriteLine("Number of special characters: " + spec);
+           // Console.WriteLine("Number of letters: " + letter);
+           // Console.WriteLine("Amount of numbers: " + numeric);
+           // Console.WriteLine("Number of special characters: " + spec);
+
+            Console.WriteLine("Number of letters: " + letter + "\nAmount of numbers: " + numeric + "\nNumber of special characters: " + spec);
 
             Console.ReadLine();
         }
--- Solution_8_lesson/Program.cs
+++ Solution_8_lesson/Program.cs
@@ -13,27 +13,72 @@ namespace Solution_8_lesson
 
             if ((object)a == null || (object)b == null)
                 return false;
-            
+
             else if (a.Length != b.Length)
                 return false;
 
-            for(int i = 0; i <a.Length; i++)
+            for (int i = 0; i < a.Length; i++)
             {
                 if (a[i] != b[i])
                     return false;
             }
             return true;
-                      
+
+        }
+
+        public static (int letter, int numeric, int spec) Analyze(string input)
+        {
+            int letter = 0;
+            int numeric = 0;
+            int spec = 0;
+
+            foreach (char c in input)
+            {
+                if (char.IsLetter(c))
+                {
+                    letter++;
+                }
+                else if (char.IsDigit(c))
+                {
+                    numeric++;
+                }
+                else
+                {
+                    spec++;
+                }
+            }
+
+            return (letter, numeric, spec);
+
+        }
+
+        static string Sort(string c)
+        {
+            char[] arr = c.ToCharArray();
+            for (int i = 0; i < arr.Length; i++)
+                for (int j = 0; j < arr.Length - i - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        char temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            return new string(arr);
+
         }
 
         static void Main(string[] args)
-        {
+        {        
             string a = "real";
             string b = "Real";
+            string c = "zhytomyr";
+            string input = "dynamo100%";
 
-            bool result = Compare(a, b);
-
-            Console.WriteLine(result);
+            Console.WriteLine(Sort(c));
+            Console.WriteLine(Compare(a, b));
+            Console.WriteLine(Analyze(input));
 
             Console.ReadLine();
         }
